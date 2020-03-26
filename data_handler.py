231c6309e89cb1af263548cc0aa46c8ec37f5fe0
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-03-26 15:23:13 +0000

    Modified how the current infected are calculated and changed the csv file names to fite the new data from the GitHub repo.

--- data_handler.py
+++ data_handler.py
@@ -29,7 +29,8 @@ def load_data():
 
     directory = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/"
     # The order of the file names is important in this list.
-    files = ["time_series_19-covid-Confirmed.csv", "time_series_19-covid-Deaths.csv", "time_series_19-covid-Recovered.csv"]
+    files = ["time_series_covid19_confirmed_global.csv", "time_series_covid19_deaths_global.csv",
+             "time_series_covid19_recovered_global.csv"]
     # Create a list of paths from the file names.
     paths = [os.path.join(directory, f) for f in files]
 
@@ -41,21 +42,34 @@ def load_data():
 
     return data_frames # Confirmed, Dead and Recovered.
 
+def load_updated_data():
+    get_github_updates()
+    return load_data()
+
 # Infected people = confirmed - (dead + recovered) {the time series for each country/region must be summed for a total}
-def calculate_total_infected(confirmed, dead, recovered):
+def calculate_total_infected(raw_data):
+    confirmed, dead, recovered = raw_data
     c_sum = confirmed.sum()[2:] # The first two rows are the sums of Long and Lat which must be removed.
     d_sum = dead.sum()[2:]
     r_sum = recovered.sum()[2:]
-    return (c_sum - (d_sum + r_sum)) # pandas Series.
 
-# display plot of newly calculated time series.
-def plot_infected(total_cases):
-    total_cases.plot.line()
-    plt.show()
+    # Find the size of the smallest series.
+    new_size = min(len(c_sum), len(d_sum), len(r_sum))
+
+    # Trim the series so they all have the same size.
+    c_sum = c_sum[:new_size].to_frame() 
+    d_sum = d_sum[:new_size].to_frame() 
+    r_sum = r_sum[:new_size].to_frame()  
+
+    # r_sum uses m/dd/yyyy this needs to be converted to m/dd/yy to match the other two sets.
+    r_sum = r_sum.set_index(c_sum.index)
+
+    return (c_sum - (d_sum + r_sum)) # pandas DataFrame.
 
 if __name__ == "__main__":
-    get_github_updates()
-    confirmed, dead, recovered = load_data()
-    total_cases = calculate_total_infected(confirmed, dead, recovered)
-    plot_infected(total_cases)
+    raw_data = load_data()
+    current_infected = calculate_total_infected(raw_data)
+    print(current_infected)
+    plot_df_line(current_infected)
+    plt.show()
 
