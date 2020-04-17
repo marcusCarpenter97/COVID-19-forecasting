5e934a12291503e03ba2a997994abbd7144fb028
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-04-17 15:49:46 +0100

    Updates test file with new preprocessing method.

--- test_data.py
+++ test_data.py
@@ -3,68 +3,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import data_handler
 
-# difference dataset
-def difference(data, interval):
-        return [data[i] - data[i - interval] for i in range(interval, len(data))]
+forecast_horizon = 4   # Number of observations to be used to predict the next event.
+train_set_ratio = 0.7  # The size of the training set as a percentage of the data set.
 
-# invert difference
-def invert_difference(orig_data, diff_data, interval=1):
-        return [diff_data[i-interval] + orig_data[i-interval] for i in range(interval, len(orig_data))]
+# Setup data.
+raw_data = data_handler.load_data()
+current_infected = data_handler.calculate_total_infected(raw_data)
 
-def reverse_difference(original, diff_predictions):
-    diff_predictions.reset_index(drop=True, inplace=True)  # Convert the inde column from dates to ints.
-    return pd.DataFrame([original + sum(diff_predictions[:t+1][0]) for t, _ in diff_predictions.iterrows()]) # un-diff predictions
+# Split the data into train and test sets.
+train, test = data_handler.split_train_test(current_infected, train_set_ratio)
 
-def plot(data_to_plot):
-    print(f"{data_to_plot}")
-    print(type(data_to_plot))
+# Make the time series data stationary.
+train_log, train_diff, stationary_train = data_handler.adjust_data(train)
+test_log, test_diff, stationary_test = data_handler.adjust_data(test)
 
-    fig = plt.figure() 
-    plt.plot(data_to_plot)
-    return fig
+# Transform the data into a supervised learning dataset.
+supervised_train = data_handler.series_to_supervised(stationary_train, 0, forecast_horizon)
+supervised_test = data_handler.series_to_supervised(stationary_test, 0, forecast_horizon)
 
-if __name__ == "__main__":
-    raw_data = data_handler.load_data()
+# Create sets for input and output based on the forecast horizon.
+x_train, y_train = data_handler.split_horizon(supervised_train, forecast_horizon)
+x_test, y_test = data_handler.split_horizon(supervised_test, forecast_horizon)
 
-    current_infected = data_handler.calculate_total_infected(raw_data)
-    print(f"og   {len(list(current_infected[0]))}")
-    original_fig = plot(current_infected)
+# Rescale data.
+scaled_train = data_handler.rescale_data(y_train, train_diff[0], train_log[0], forecast_horizon)
+scaled_test = data_handler.rescale_data(y_test, test_diff[0], test_log[0], forecast_horizon)
 
-    log_cases = pd.DataFrame(np.log(current_infected))
-    print(f"log   {len(list(log_cases[0]))}")
-    log_fig = plot(log_cases)
+# Plot data.
+#cur_inf_fig = data_handler.plot(current_infected)
+#train_fig = data_handler.plot(train)
+#test_fig = data_handler.plot(test)
+#stat_train_fig = data_handler.plot(stationary_train)
+#stat_test_fig = data_handler.plot(stationary_test)
 
-    first_diff = log_cases.diff().dropna()
-    print(f"diff1   {len(list(first_diff[0]))}")
-    first_fig = plot(first_diff)
+#strain_fig = data_handler.plot(scaled_train)
+#stest_fig = data_handler.plot(scaled_test)
 
-    stationary_data = first_diff.diff().dropna()
-    print(f"stat   {len(list(stationary_data[0]))}")
-    stationary_fig = plot(stationary_data)
+# The first sample is lost after each differencing, so the + 2 is required. 
+empty_arr = np.empty((forecast_horizon+2, 1))
+empty_arr[:] = np.nan
+og = np.concatenate([empty_arr, scaled_train, empty_arr, scaled_test])
 
-    station_to_diff = pd.DataFrame(invert_difference(list(first_diff[0]), list(stationary_data[0])))
-    print(f"to_diff   {len(list(station_to_diff[0]))}")
-    to_diff_fig = plot(station_to_diff)
+fig = plt.figure() 
+plt.plot(current_infected)
+plt.plot(og)
 
-    diff_to_log = pd.DataFrame(invert_difference(list(log_cases[0][1:]), list(station_to_diff[0])))
-    print(f"to_log   {len(list(diff_to_log[0]))}")
-    to_log_fig = plot(diff_to_log)
-
-    log_to_og = pd.DataFrame(np.exp(diff_to_log))
-    print(f"to_og   {len(list(log_to_og[0]))}")
-    to_og_fig = plot(log_to_og)
-
-    print("Original vs log_to_og")
-    print(pd.concat([current_infected.reset_index(drop=1).add_suffix('_1'),
-                    log_to_og.reset_index(drop=1).add_suffix('_2')], axis=1).fillna(''))
-
-    print("Log vs diff_to_log")
-    print(pd.concat([log_cases.reset_index(drop=1).add_suffix('_1'),
-                    diff_to_log.reset_index(drop=1).add_suffix('_2')], axis=1).fillna(''))
-
-    print("First diff vs station_to_diff")
-    print(pd.concat([first_diff.reset_index(drop=1).add_suffix('_1'),
-                    station_to_diff.reset_index(drop=1).add_suffix('_2')], axis=1).fillna(''))
-
-    plt.show()
-
+plt.show()
