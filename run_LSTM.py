ca9fc5ad22a3c643a53e952e9a788d2081c9029d
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-04-17 15:49:12 +0100

    Hopefully reduced data leakage by improving how the data preprocessing works.

--- run_LSTM.py
+++ run_LSTM.py
@@ -6,34 +6,26 @@ import LSTM
 import data_handler
 
 forecast_horizon = 4   # Number of observations to be used to predict the next event.
-train_set_size = 0.67  # The size of the training set as a percentage of the data set.
+train_set_ratio = 0.7  # The size of the training set as a percentage of the data set.
 
-#Load in the data most recent data available on GitHub.
+# Setup data.
 raw_data = data_handler.load_data()
-
-# Calculate the current total cases from the data.
 current_infected = data_handler.calculate_total_infected(raw_data)
 
-# First the natural logarithm of the original data is taken.
-log_cases = pd.DataFrame(np.log(current_infected))
-
-# Then the difference between each point in the data is calculated.
-first_diff = log_cases.diff().dropna()
-
-# As the first differencing did not provide good results the difference of the difference is taken.
-stationary_data = first_diff.diff().dropna()
-
-# Convert the time series to a supervised learning dataset.
-# In this setting four days (input) of data will be used to forecast the fith day (output).
-supervised_data = data_handler.series_to_supervised(stationary_data, 0, forecast_horizon)
-
-# Split the data into input (x) and output (y).
-x, y = data_handler.split_data(supervised_data, forecast_horizon)
-
 # Split the data into train and test sets.
-train_size = int(x.shape[0] * train_set_size)
-x_train, x_test = x[:train_size], x[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
+train, test = data_handler.split_train_test(current_infected, train_set_ratio)
+
+# Make the time series data stationary.
+train_log, train_diff, stationary_train = data_handler.adjust_data(train)
+test_log, test_diff, stationary_test = data_handler.adjust_data(test)
+
+# Transform the data into a supervised learning dataset.
+supervised_train = data_handler.series_to_supervised(stationary_train, 0, forecast_horizon)
+supervised_test = data_handler.series_to_supervised(stationary_test, 0, forecast_horizon)
+
+# Create sets for input and output based on the forecast horizon.
+x_train, y_train = data_handler.split_horizon(supervised_train, forecast_horizon)
+x_test, y_test = data_handler.split_horizon(supervised_test, forecast_horizon)
 
 # Reshape x from [samples, time steps] to [samples, time steps, features]
 # Where samples = rows and time steps = columns.
@@ -51,25 +43,28 @@ print("Done")
 train_prediction = lstm.predict(x_train)
 test_prediction = lstm.predict(x_test)
 
-#diff_list = list(first_diff[0])
-#log_list = list(log_cases[0])
+# Rescale predictions.
+scaled_train_p = data_handler.rescale_data(train_prediction, train_diff[0], train_log[0], forecast_horizon)
+scaled_test_p = data_handler.rescale_data(test_prediction, test_diff[0], test_log[0], forecast_horizon)
+# Rescale answers.
+scaled_train = data_handler.rescale_data(y_train, train_diff[0], train_log[0], forecast_horizon)
+scaled_test = data_handler.rescale_data(y_test, test_diff[0], test_log[0], forecast_horizon)
 
-predictions = np.concatenate((train_prediction, test_prediction))
-#scaled_predictions = data_handler.rescale_data(predictions, diff_list, log_list, forecast_horizon)
-scaled_predictions = data_handler.rescale_data(predictions, first_diff[0], log_cases[0], forecast_horizon)
-
-#scaled_y = data_handler.rescale_data(y, diff_list, log_list, forecast_horizon)
-scaled_y = data_handler.rescale_data(y, first_diff[0], log_cases[0], forecast_horizon)
-
-train_rmse = lstm.rmse(y_train, train_prediction)
-test_rmse = lstm.rmse(y_test, test_prediction)
-total_rmse = lstm.rmse(scaled_y, scaled_predictions).to_numpy()  # Because the inputs are DataFrames.
+train_rmse = lstm.rmse(scaled_train, scaled_train_p)
+test_rmse = lstm.rmse(scaled_test, scaled_test_p)
 print(f"Train RMSE : {train_rmse}")
 print(f"Test RMSE : {test_rmse}")
-print(f"Total RMSE : {total_rmse[0]}")
+
+# The first sample is lost after each differencing, so the + 2 is required. 
+empty_arr = np.empty((forecast_horizon+2, 1))
+empty_arr[:] = np.nan
+# Join train and test predictions to create one curve.
+predictions = np.concatenate([empty_arr, scaled_train_p, empty_arr, scaled_test_p])
+# Plot it over the original data.
 
 fig, ax = plt.subplots()
-ax.plot(scaled_y, label='Original data')
-ax.plot(scaled_predictions, label='Forecasts')
+ax.plot(current_infected, label='Original data')
+ax.plot(predictions, label='Forecasts')
 legend = ax.legend(loc='best')
+plt.xticks(rotation=90)
 plt.show()
