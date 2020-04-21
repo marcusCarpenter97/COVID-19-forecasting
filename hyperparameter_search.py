dfe0bc323914c5f4282600af740c3bbc5f15bd62
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-04-21 16:24:32 +0100

    Implemented Taguchi selection.

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -1,4 +1,5 @@
 import time
+import pandas as pd
 import numpy as np
 from timeit import timeit
 from random import seed, choice, randrange, uniform, sample
@@ -12,7 +13,7 @@ node_step = 5
 loss_funcs = ['mean_squared_error', 'mean_absolute_error', 'mean_absolute_percentage_error', 'mean_squared_logarithmic_error']
 dropout_min = 0.1
 dropout_max = 0.5
-dropout_step = 0.1
+dropout_points = 5  # How many points between min and max
 activations = ['tanh', 'sigmoid', 'relu', 'swish']
 
 def truncate(f):
@@ -43,15 +44,55 @@ def no_repeats(sample_count):
 def generate_all_samples():
     results = []
     for node_count in range(node_min, node_max, node_step):
-        for dropout in np.linspace(dropout_min, dropout_max, 9):
+        for dropout in np.linspace(dropout_min, dropout_max, dropout_points):
             for loss in loss_funcs:
                 for lstm_activation in activations:
                     for dense_activation in activations:
                         results.append((node_count, dropout, loss, lstm_activation, dense_activation))
     return results
 
-def select_random_hyperparameters(num_of_samples):
-    return sample(generate_all_samples(), num_of_samples)
+def get_taguchi_samples(target):
+    ret_vals = []
+    ret_vals.append(target[0])  # First element
+    ret_vals.append(target[int(len(target)/4)])  # Quarter of list
+    ret_vals.append(target[int(len(target)/4)+int(len(target)/2)])  # 3/4 of list
+    ret_vals.append(target[-1])  # Last element
+    return ret_vals
+
+def make_taguchi_samples():
+    taguchi_table = pd.read_csv("taguchi-L16.csv", header=None)
+    lookup_table = []
+
+    # Get the sublist for nodes and dropout.
+    all_nodes = range(node_min, node_max, node_step)
+    nodes = get_taguchi_samples(all_nodes)
+
+    all_dropouts = np.linspace(dropout_min, dropout_max, dropout_points)
+    dropouts = get_taguchi_samples(all_dropouts)
+
+    # Create lookup table.
+    lookup_table.append(nodes)
+    lookup_table.append(dropouts)
+    lookup_table.append(loss_funcs)
+    lookup_table.append(activations)  # For hidden layer.
+    lookup_table.append(activations)  # For dense.
+    
+    ret_table = []
+    for _, row in taguchi_table.iterrows():
+        temp_row = []
+        for idx, val in enumerate(row):
+            temp_row.append(lookup_table[idx][val-1])  # Taguchi values start at 1.
+        ret_table.append(tuple(temp_row))
+    return ret_table
+
+def select_hyperparameters(num_of_samples=10, method=None):
+    if method == "random":
+        return sample(generate_all_samples(), num_of_samples)
+    elif method == "taguchi":
+        return make_taguchi_samples()
+    else:
+        print(f"Invalid method. Should be 'random' or 'taguchi' got {method}")
+        raise SystemExit
 
 def all_samples_while():
     results = []
