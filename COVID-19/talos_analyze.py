Fixed how the test saves the results. Also fixed the data scaling used to train and test the models.

COVID-19/talos_analyze.py
@@ -35,24 +35,20 @@ def weighted_pinball_loss(prediction, target, quantiles, fatal_weight=False):
     pass
 
 # Univariate data
-covid_data = data.Data()
-covid_data.calculate_current_infected()
-covid_data.split_train_test(covid_data.current_infected)
-covid_data.prepare_data()
-covid_data.series_to_supervised(FORECAST_HORIZON)
-covid_data.split_in_from_out(FORECAST_HORIZON, UNIVARIATE)
-UNI_X_TRAIN = covid_data.x_train
-UNI_X_TEST = covid_data.x_test
-UNI_Y_TRAIN, UNI_Y_TEST = covid_data.rescale_data(covid_data.y_train, covid_data.y_test, FORECAST_HORIZON)
+uni_covid_data = data.Data()
+uni_covid_data.calculate_current_infected()
+uni_covid_data.split_train_test(uni_covid_data.current_infected)
+uni_covid_data.prepare_data()
+uni_covid_data.series_to_supervised(FORECAST_HORIZON)
+uni_covid_data.split_in_from_out(FORECAST_HORIZON, UNIVARIATE)
+UNI_Y_TRAIN, UNI_Y_TEST = uni_covid_data.rescale_data(uni_covid_data.y_train, uni_covid_data.y_test, FORECAST_HORIZON)
 # Multivariate data
-covid_data = data.Data()
-covid_data.make_kaggle_data()
-covid_data.split_train_test(covid_data.kaggle_data)
-covid_data.prepare_data()
-covid_data.series_to_supervised(FORECAST_HORIZON, multivariate=True)
-MULTI_X_TRAIN = covid_data.x_train
-MULTI_X_TEST = covid_data.x_test
-MULTI_Y_TRAIN, MULTI_Y_TEST = covid_data.rescale_data(covid_data.y_train, covid_data.y_test, FORECAST_HORIZON)
+multi_covid_data = data.Data()
+multi_covid_data.make_kaggle_data()
+multi_covid_data.split_train_test(multi_covid_data.kaggle_data)
+multi_covid_data.prepare_data()
+multi_covid_data.series_to_supervised(FORECAST_HORIZON, multivariate=True)
+MULTI_Y_TRAIN, MULTI_Y_TEST = multi_covid_data.rescale_data(multi_covid_data.y_train, multi_covid_data.y_test, FORECAST_HORIZON)
 
 # Load talos results.
 lstm_res = talos.Analyze('lstm.csv')
@@ -80,7 +76,7 @@ print(top_multi_lstm)
 print('Multi GRU results')
 print(top_multi_gru)
 
-def test_top_res(model_name, top_res, model, x_train, y_train, x_test, y_test):
+def test_top_res(covid_data, model_name, top_res, model, scaled_train, scaled_test):
     for idx, row in top_res.iterrows():
         print(idx)
         rmse_train = []
@@ -92,33 +88,33 @@ def test_top_res(model_name, top_res, model, x_train, y_train, x_test, y_test):
         for i in range(10):  # Number of tests was chosen arbitrarily.
             print(i)
             # Create and train the model.
-            hist, trained_model = model(x_train, y_train, [], [], row)
+            hist, trained_model = model(covid_data.x_train, covid_data.y_train, [], [], row)
             # Make predictions.
-            train_pred = trained_model.predict(x_train)
-            test_pred = trained_model.predict(x_test)
+            train_pred = trained_model.predict(covid_data.x_train)
+            test_pred = trained_model.predict(covid_data.x_test)
             # Rescale predictions to original scale.
-            scaled_train, scaled_test = covid_data.rescale_data(train_pred, test_pred, FORECAST_HORIZON)
+            scaled_train_pred, scaled_test_pred = covid_data.rescale_data(train_pred, test_pred, FORECAST_HORIZON)
 
             # Calculate error values for train and test data.
-            rmse_train.append(rmse(scaled_train, y_train))
-            rmse_test.append(rmse(scaled_test, y_test))
+            rmse_train.append(rmse(scaled_train_pred, scaled_train))
+            rmse_test.append(rmse(scaled_test_pred, scaled_test))
 
-            rmsle_train.append(rmsle(scaled_train, y_train))
-            rmsle_test.append(rmsle(scaled_test, y_test))
+            rmsle_train.append(rmsle(scaled_train_pred, scaled_train))
+            rmsle_test.append(rmsle(scaled_test_pred, scaled_test))
 
-            mase_train.append(mase(scaled_train, y_train, len(x_train)))
-            mase_test.append(mase(scaled_test, y_test, len(x_train)))
+            mase_train.append(mase(scaled_train_pred, scaled_train, len(covid_data.x_train)))
+            mase_test.append(mase(scaled_test_pred, scaled_test, len(covid_data.x_train)))
 
-            # Generate performance report for model.
-            report = (f"\n\nModel name: {model_name}\n RMSE on train: {rmse_train}\n RMSE on test: {rmse_test}\n Average RMSE: "
-            "{np.array(rmse_test).mean()}\n RMSLE on train: {rmsle_train}\n RMSLE on test: {rmsle_test}\n Average RMSLE: "
-            "{np.array(rmsle_test).mean()}\n MASE on train: {mase_train}\n MASE on test: {mase_test}\n Average MASE: "
-            "{np.array(mase_test).mean()}\n Hyperparameters used: {row}\n")
+        # Generate performance report for model.
+        report = (f"\n\nModel name: {idx}\n RMSE on train: {rmse_train}\n RMSE on test: {rmse_test}\n Average RMSE: "
+        f"{np.array(rmse_test).mean()}\n RMSLE on train: {rmsle_train}\n RMSLE on test: {rmsle_test}\n Average RMSLE: "
+        f"{np.array(rmsle_test).mean()}\n MASE on train: {mase_train}\n MASE on test: {mase_test}\n Average MASE: "
+        f"{np.array(mase_test).mean()}\n Hyperparameters used:\n{row}\n")
 
-            with open(f"{model_name}.txt", 'a') as res_file:
-                res_file.write(report)
+        with open(f"{model_name}.txt", 'a') as res_file:
+            res_file.write(report)
 
-test_top_res("lstm", top_lstm, talos_test.lstm_current_infected, UNI_X_TRAIN, UNI_Y_TRAIN, UNI_X_TEST, UNI_Y_TEST)
-test_top_res("gru", top_gru, talos_test.gru_current_infected, UNI_X_TRAIN, UNI_Y_TRAIN, UNI_X_TEST, UNI_Y_TEST)
-test_top_res("multi_lstm", top_multi_lstm, talos_test.multivariate_lstm, MULTI_X_TRAIN, MULTI_Y_TRAIN, MULTI_X_TEST, MULTI_Y_TEST)
-test_top_res("multi_gru", top_multi_gru, talos_test.multivariate_gru, MULTI_X_TRAIN, MULTI_Y_TRAIN, MULTI_X_TEST, MULTI_Y_TEST)
+test_top_res(uni_covid_data, "lstm", top_lstm, talos_test.lstm_current_infected, UNI_Y_TRAIN, UNI_Y_TEST)
+test_top_res(uni_covid_data, "gru", top_gru, talos_test.gru_current_infected, UNI_Y_TRAIN, UNI_Y_TEST)
+test_top_res(multi_covid_data, "multi_lstm", top_multi_lstm, talos_test.multivariate_lstm, MULTI_Y_TRAIN, MULTI_Y_TEST)
+test_top_res(multi_covid_data, "multi_gru", top_multi_gru, talos_test.multivariate_gru, MULTI_Y_TRAIN, MULTI_Y_TEST)