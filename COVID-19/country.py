2f4d1e3fc27564aec40e3597b7f6d8e5c909b0d1
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-05-15 15:06:07 +0100

    Created class to store data for each individual country.

---
+++ COVID-19/country.py
@@ -0,0 +1,17 @@
+
+class Country:
+    """ Represents a country with COVID-19 """
+
+    def __init__(self, n, p, c=None, d=None, r=None):
+        self.name = n
+        self.popultion = p['Population']
+        self.confirmed = c.sum(numeric_only=True)
+        self.deceased = d.sum(numeric_only=True)
+        self.recovered = r.sum(numeric_only=True)
+
+    def print_country(self):
+        print(self.name)
+        print(self.popultion)
+        print(self.confirmed)
+        print(self.deceased)
+        print(self.recovered)
