e5dc661d42f08a702c895986c5e7de15b866697d
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-09-18 14:36:57 +0100

    Added comments and doc strings to the code and changed from using math to numpy functions.

--- COVID-19/data.py
+++ COVID-19/data.py
@@ -1,19 +1,27 @@
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import one_hot
-import math
 import numpy as np
 import pandas as pd
 import data_loader
 import country
 
 class Data:
-    """ A container for what the data_loader loads."""
+    """ A container that stores the time series COVID-19 data for each country.
+        The data is loaded from the data_loader module.
+    """
     def __init__(self):
         self.raw_confirmed, self.raw_deceased, self.raw_recovered = data_loader.load_covid_data()
         self.population = data_loader.load_population_data()
         self.countries = []
 
         def generate_world_data():
+            """
+                Creates the World data by summing all the individual
+                time series data for all the countries in the dataset.
+
+                Returns:
+                    A Country object containing the world data.
+            """
             p = self.population.sum(numeric_only=True)['Population']
             c = self.raw_confirmed.sum(numeric_only=True)
             d = self.raw_deceased.sum(numeric_only=True)
@@ -23,6 +31,11 @@ class Data:
             return country.Country("World", p, c, d, r, i, h)
 
         def generate_countries():
+            """
+                Creates a list of Country objects each containing the COVID-19 data for their respective country.
+                Retunrs:
+                    A list of Country objects.
+            """
             countries = []
             # For each country in population.
             for name, pop in self.population.iterrows():
@@ -42,12 +55,13 @@ class Data:
 
     def find_country(self, name):
         """
-        Takes a country's name and returns its object object.
+        Takes a country's name and returns its country object.
         Returns None if country is not found.
         """
         res = [country for country in self.countries if country.name == name]
         return None if len(res) == 0 else res[0]
 
+    # TODO ?
     def encode_names(self, extra_size=1.25):
         """
         Creates encoded versions of the country names.
@@ -58,7 +72,7 @@ class Data:
         max_length - int - size of biggest word.
         """
         # Idealy this should use the number of words not the country names.
-        vocab_size = math.ceil(len(self.countries) * extra_size)
+        vocab_size = np.ceil(len(self.countries) * extra_size)
 
         encoded = [one_hot(country.name, vocab_size) for country in self.countries]
         max_length = len(max(encoded, key=lambda x: len(x)))
@@ -73,15 +87,20 @@ class Data:
     def calculate_current_infected(self, c, d, r):
         """
         Infected people = confirmed - (dead + recovered)
+        Params: all Series objects.
+        Returns: Series.
         """
         return c - (d + r)
 
     def calculate_healthy(self, p, d, r, i):
         """
         Healthy people = population - (dead + recovered + infected)
+        Params: all Series objects.
+        Returns: Series.
         """
         return p - (d + r + i)
 
+    # TODO ?
     def log(self):
         """
         Apply the log function on all countries.
@@ -89,6 +108,7 @@ class Data:
         for country in self.countries:
             country.log_data()
 
+    # TODO ?
     def exp(self):
         """
         Apply the exp function on all countries.
@@ -96,6 +116,7 @@ class Data:
         for country in self.countries:
             country.exp_data()
 
+    # TODO ?
     def difference(self):
         """
         Apply the diff function on all countries.
@@ -103,6 +124,7 @@ class Data:
         for country in self.countries:
             country.diff_data()
 
+    # TODO ?
     def integrate(self):
         """
         Integrate the data for all countries.
@@ -110,6 +132,7 @@ class Data:
         for country in self.countries:
             country.int_data()
 
+    # TODO !
     def split_train_test(self, test_size, horizon):
         """
         Create training and testing datasets for all countries.
@@ -117,6 +140,7 @@ class Data:
         for country in self.countries:
             country.split_data(test_size, horizon)
 
+    # TODO !
     def supervise_data(self, horizon):
         """
         Create input and output test sets for all countries.
@@ -125,12 +149,12 @@ class Data:
             country.supervise_data(horizon)
 
     def print_n_plot_country(self, name, bars=[]):
-        """ 
+        """
         Display a country's data.
 
         Parametes
         country: str - a countries name.
-        bars: list - list of x coordinates 
+        bars: list - list of x coordinates
         defining where to place vertical bars.
         """
         country = self.find_country(name)
