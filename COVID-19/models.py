ffb85a9981cf4ab239fea6e373349a637bd6fb1e
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-09-20 19:09:45 +0100

    Fix multi output model so that now it produces the correct number of outputs based on the specified time steps.

--- COVID-19/models.py
+++ COVID-19/models.py
@@ -2,21 +2,20 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def RNNMultiOutput(temporal_input_shape, word_input_shape, units, name, layer, activation):
+def RNNMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     """
     """
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
     word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
 
-    hidden_rnn = layer(units, activation=activation, return_sequences=True, name=f"{name}_encoder")(temporal_inputs)
+    hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
     hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
-    hidden_dense = layers.RepeatVector(temporal_input_shape[0], name="country_name_rep")(hidden_dense)
 
     context = layers.concatenate([hidden_rnn, hidden_dense], name="context")
 
-    confimed_out = layers.TimeDistributed(layers.Dense(1), name="confirmed")(context)
-    deceased_out = layers.TimeDistributed(layers.Dense(1), name="deceased")(context)
-    recovered_out = layers.TimeDistributed(layers.Dense(1), name="recovered")(context)
+    confimed_out = layers.Dense(output_size, name="confirmed")(context)
+    deceased_out = layers.Dense(output_size, name="deceased")(context)
+    recovered_out = layers.Dense(output_size, name="recovered")(context)
 
     model = keras.Model(inputs=[temporal_inputs, word_inputs], outputs=[confimed_out, deceased_out, recovered_out], name =
                         f"{name}MultiOutput")
@@ -25,18 +24,18 @@ def RNNMultiOutput(temporal_input_shape, word_input_shape, units, name, layer, a
                   metrics=[tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.RootMeanSquaredError()])
     return model
 
-def LSTMMultiOutput(temporal_input_shape, word_input_shape, units, activation='relu'):
+def LSTMMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
-    return RNNMultiOutput(temporal_input_shape, word_input_shape, units, name, layer, activation)
+    return RNNMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
 def LSTMSingleOutput():
     pass
 
-def GRUMultiOutput(temporal_input_shape, word_input_shape, units, activation='relu'):
+def GRUMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "GRU"
     layer = layers.GRU
-    return RNNMultiOutput(temporal_input_shape, word_input_shape, units, name, layer, activation)
+    return RNNMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
 def GRUSingleOutput():
     pass
