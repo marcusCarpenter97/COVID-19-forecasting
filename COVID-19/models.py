f6d24b55a813dba9eb8f87ceac11ad5a788b7984
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-09-26 13:54:42 +0100

    Revert "Removed word shape from inputs and models as it is no longer necessary due to representing the country names as hash values." The hash values are now represented as numpy arrays again.
    This reverts commit 1e0bdcc162a06300208b39a3708200e77cda12e5.

--- COVID-19/models.py
+++ COVID-19/models.py
@@ -3,12 +3,12 @@ import tensorflow_addons as tfa
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def RNNMultiOutput(temporal_input_shape, recurrent_units, output_size, name, layer, activation):
+def RNNMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     """
     Individual weights.
     """
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
-    word_inputs = keras.Input(shape=1, name="country_name_input")
+    word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
 
     hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
     hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
@@ -26,12 +26,12 @@ def RNNMultiOutput(temporal_input_shape, recurrent_units, output_size, name, lay
                   metrics=[tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.RootMeanSquaredError()])
     return model
 
-def RNNMultiOutput_V2(temporal_input_shape, recurrent_units, output_size, name, layer, activation):
+def RNNMultiOutput_V2(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     """
     Shared weights.
     """
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
-    word_inputs = keras.Input(shape=1, name="country_name_input")
+    word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
 
     hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
     hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
@@ -50,9 +50,9 @@ def RNNMultiOutput_V2(temporal_input_shape, recurrent_units, output_size, name, 
                   metrics=[tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.RootMeanSquaredError()])
     return model
 
-def RNNSingleOutput(temporal_input_shape, recurrent_units, output_size, name, layer, activation):
+def RNNSingleOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
-    word_inputs = keras.Input(shape=1, name="country_name_input")
+    word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
 
     hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
     hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
@@ -68,13 +68,13 @@ def RNNSingleOutput(temporal_input_shape, recurrent_units, output_size, name, la
                   metrics=[tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.RootMeanSquaredError()])
     return model
 
-def RNNSingleOutputQuantile(temporal_input_shape, recurrent_units, output_size, name, layer, activation):
+def RNNSingleOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     """
     One output node for each quantile. Each output node produces all features.
     e.g. 3 quantile and 3 features = 3 outputs (each has 3 features), one for each quantile.
     """
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
-    word_inputs = keras.Input(shape=1, name="country_name_input")
+    word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
 
     hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
     hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
@@ -100,13 +100,13 @@ def RNNSingleOutputQuantile(temporal_input_shape, recurrent_units, output_size, 
     model.compile(optimizer=tf.keras.optimizers.Adam(), loss=losses, metrics=metrics)
     return model
 
-def RNNMultiOutputQuantile(temporal_input_shape, recurrent_units, output_size, name, layer, activation):
+def RNNMultiOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     """
     Each output node produces the values for a featue at a quantile.
     e.g. 3 quantile and 3 features = 9 outputs.
     """
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
-    word_inputs = keras.Input(shape=1, name="country_name_input")
+    word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
 
     hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
     hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
@@ -148,52 +148,52 @@ def RNNMultiOutputQuantile(temporal_input_shape, recurrent_units, output_size, n
 
     return model
 
-def LSTMMultiOutput(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def LSTMMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
-    return RNNMultiOutput(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def LSTMSingleOutput(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def LSTMSingleOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
-    return RNNSingleOutput(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNSingleOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def LSTMMultiOutput_V2(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def LSTMMultiOutput_V2(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
-    return RNNMultiOutput_V2(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNMultiOutput_V2(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def LSTMMultiOutputQuantile(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def LSTMMultiOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
-    return RNNMultiOutputQuantile(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNMultiOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def LSTMSingleOutputQuantile(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def LSTMSingleOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
-    return RNNSingleOutputQuantile(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNSingleOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def GRUMultiOutput(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def GRUMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "GRU"
     layer = layers.GRU
-    return RNNMultiOutput(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def GRUSingleOutput(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def GRUSingleOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "GRU"
     layer = layers.GRU
-    return RNNSingleOutput(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNSingleOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def GRUMultiOutput_V2(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def GRUMultiOutput_V2(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "GRU"
     layer = layers.GRU
-    return RNNMultiOutput_V2(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNMultiOutput_V2(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def GRUMultiOutputQuantile(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def GRUMultiOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "GRU"
     layer = layers.GRU
-    return RNNMultiOutputQuantile(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNMultiOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
 
-def GRUSingleOutputQuantile(temporal_input_shape, recurrent_units, output_size, activation='relu'):
+def GRUSingleOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "GRU"
     layer = layers.GRU
-    return RNNSingleOutputQuantile(temporal_input_shape, recurrent_units, output_size, name, layer, activation)
+    return RNNSingleOutputQuantile(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation)
