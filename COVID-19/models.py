27c35c9ee7bc2f87d41df7815e35f614d3bf2990
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-09-27 10:14:12 +0100

    Converted rnn multi out single from Functional API to custom model.

--- COVID-19/models.py
+++ COVID-19/models.py
@@ -31,29 +31,24 @@ class RNNMultiOutputIndividual(keras.Model):
         context = self.encoder(inputs)
         return self.c_out(context), self.d_out(context), self.r_out(context)
 
-def RNNMultiOutput_V2(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
+class RNNMultiOutputShared(keras.Model):
     """
-    Shared weights.
+    Multi output RNN model with shared weights on the output nodes.
     """
-    temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
-    word_inputs = keras.Input(shape=word_input_shape, name="country_name_input")
+    def __init__(self, output_size, rnn_units, rnn_layer, rnn_activation):
+        super(RNNMultiOutputShared, self).__init__()
+        self.encoder = EncoderBlock(rnn_units, rnn_layer, rnn_activation)
 
-    hidden_rnn = layer(recurrent_units, activation=activation, name=f"{name}_encoder")(temporal_inputs)
-    hidden_dense = layers.Dense(1, name="country_name")(word_inputs)
+        self.rep_vec = layers.RepeatVector(output_size)
 
-    context = layers.concatenate([hidden_rnn, hidden_dense], name="context")
-    context = layers.RepeatVector(output_size)(context)
+        self.c_out = layers.TimeDistributed(layers.Dense(1), name="confirmed")
+        self.d_out = layers.TimeDistributed(layers.Dense(1), name="deceased")
+        self.r_out = layers.TimeDistributed(layers.Dense(1), name="recovered")
 
-    confirmed_out = layers.TimeDistributed(layers.Dense(1), name="confirmed")(context)
-    deceased_out = layers.TimeDistributed(layers.Dense(1), name="deceased")(context)
-    recovered_out = layers.TimeDistributed(layers.Dense(1), name="recovered")(context)
-
-    model = keras.Model(inputs=[temporal_inputs, word_inputs], outputs=[confirmed_out, deceased_out, recovered_out], name =
-                        f"{name}MultiOutput_V2")
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError(),
-                  metrics=[tf.keras.metrics.MeanSquaredError(), tf.keras.metrics.RootMeanSquaredError()])
-    return model
+    def call(self, inputs):
+        context = self.encoder(inputs)
+        context = self.rep_vec(context)
+        return self.c_out(context), self.d_out(context), self.r_out(context)
 
 def RNNSingleOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, name, layer, activation):
     temporal_inputs = keras.Input(shape=temporal_input_shape, name="time_series_input")
@@ -162,6 +157,7 @@ def fitModel(my_model, x, y, epochs, verbose=2):
 def evaluateModel(my_model, x, y, verbose=1, return_dict=True):
     return my_model.evaluate(x=x, y=y, verbose=verbose, return_dict=return_dict)
 
+# TODO remove.
 def LSTMMultiOutput(temporal_input_shape, word_input_shape, recurrent_units, output_size, activation='relu'):
     name = "LSTM"
     layer = layers.LSTM
