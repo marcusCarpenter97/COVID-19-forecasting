9ec170439a4b4d84a02e8d9afdf94a5d4f159030
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-10-07 16:16:21 +0100

    Made predictions for multi out indv gru and lstm.

--- COVID-19/run_COVID-19_models.py
+++ COVID-19/run_COVID-19_models.py
@@ -54,19 +54,20 @@ else:
     print_eval_res(multi_quantile_out_eval)
     print_eval_res(single_quantile_out_eval)
 
-# Predictions using best model on a selection of countries.
-mulit_out_pred = MULTI_MODELS["multiOutIndvGru"].predict([test_x, enc_names])
-mulit_out_pred = np.stack(mulit_out_pred)
+# Make predictions using the selected models.
+gru_mulit_out_pred = MULTI_MODELS["multiOutIndvGru"].predict([test_x, enc_names])
+gru_mulit_out_pred = np.stack(gru_mulit_out_pred)
+
+lstm_mulit_out_pred = MULTI_MODELS["multiOutIndvLstm"].predict([test_x, enc_names])
+lstm_mulit_out_pred = np.stack(lstm_mulit_out_pred)
 
 # Reshape the predictions to fit the rescaler.
-predictions = []
-for c, d, r in zip(mulit_out_pred[0], mulit_out_pred[1], mulit_out_pred[2]):
-    predictions.append(np.stack([c, d, r]).T)
-
-predictions = np.stack(predictions)
+gru_predictions = reshape_predictions(gru_mulit_out_pred)
+lstm_predictions = reshape_predictions(lstm_mulit_out_pred)
 
 # De-standarize predictions and data.
-rescaled_predictions = COVID_DATA.destandarize_data(predictions)
+rescaled_gru_predictions = COVID_DATA.destandarize_data(gru_predictions)
+rescaled_lstm_predictions = COVID_DATA.destandarize_data(lstm_predictions)
 
 # Plot predictions vs data.
 def prepare_country_data_for_plots(name, enc_names, prediction_data):
@@ -76,8 +77,12 @@ def prepare_country_data_for_plots(name, enc_names, prediction_data):
     country_predictions = country_predictions.reshape(country_predictions.shape[1], country_predictions.shape[2]).T
     return country_predictions, country.test_y.T
 
-for country_name in EXAMPLE_COUTRIES:
-    predictions, test_y = prepare_country_data_for_plots(country_name, enc_names, rescaled_predictions)
-    plot_pred_v_data(predictions, test_y, country_name)
+def make_plots_for_model(model_predictions, model_name):
+    for country_name in EXAMPLE_COUTRIES:
+        predictions, test_y = prepare_country_data_for_plots(country_name, enc_names, model_predictions)
+        plot_pred_v_data(predictions, test_y, country_name, model_name)
+
+make_plots_for_model(rescaled_gru_predictions, "multiOutIndvGru")
+make_plots_for_model(rescaled_lstm_predictions, "multiOutIndvLstm")
 
 plt.show()
