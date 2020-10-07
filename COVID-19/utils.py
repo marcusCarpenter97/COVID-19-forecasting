dfe10f30be7e819257d0569d17c34fb9152de6a9
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-10-07 16:13:57 +0100

    Moved reshape predictions method to utils and added model name to plots.

--- COVID-19/utils.py
+++ COVID-19/utils.py
@@ -1,5 +1,6 @@
 """ Utility module woth some helper functions. """
 import os
+import numpy as np
 from pprint import pprint
 from tensorflow.keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
@@ -61,14 +62,14 @@ def print_eval_res(eval_dict):
         pprint(eval_dict[model_res])
         print()
 
-def plot_pred_v_data(pred, og_data, country_name):
+def plot_pred_v_data(pred, og_data, country_name, model_name):
     """
     params:
     returns:
     """
     sub_titles = ["Confirmed", "Deceased", "Recovered"]
     fig, axes = plt.subplots(1, 3, constrained_layout=True)
-    fig.suptitle(country_name)
+    fig.suptitle(f"{country_name} - {model_name}")
     for idx, ax in enumerate(axes):
         ax.plot(pred[idx])
         ax.plot(og_data[idx])
@@ -76,3 +77,9 @@ def plot_pred_v_data(pred, og_data, country_name):
         ax.set_xlabel("Days")
         ax.set_ylabel("People")
         ax.legend(["Model predictions", "Real data"], loc="best")
+
+def reshape_predictions(mulit_out_pred):
+    predictions = []
+    for c, d, r in zip(mulit_out_pred[0], mulit_out_pred[1], mulit_out_pred[2]):
+        predictions.append(np.stack([c, d, r]).T)
+    return np.stack(predictions)
