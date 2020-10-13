a98006f2b6779bd43157d04e0379fede070ecafc
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2020-10-13 13:51:39 +0100

    Removed the meed for the CrossValidate class.

--- COVID-19/cross_val_experiments.py
+++ COVID-19/cross_val_experiments.py
@@ -1,16 +1,24 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import data
-import cross_validation
 
 PAD_VAL = -10000
 H = 28
 F = 3
 PADDING = np.full((H, F), PAD_VAL)
-
 D = data.Data()
-cv = cross_validation.CrossValidate(D, H)
-train, val, test_x, test_y = cv.split_data()
+
+def split_data(data, horizon):
+    train, val, test_x, test_y = [], [], [], []
+    train_size = horizon
+    # This assumes all countries have the same length.
+    # The minus two gives space for the validation and test sets as they will overshoot.
+    k_folds = len(data.countries[0].data)//horizon - 2
+    for _ in range(k_folds):
+        tr, v, te_x, te_y = data.cross_validate(train_size, horizon)
+        train.append(tr), val.append(v), test_x.append(te_x), test_y.append(te_y)
+        train_size += horizon
+    return train, val, test_x, test_y
 
 def standardize_data(data):
     """
@@ -65,53 +73,58 @@ def prepare_output_data(data):
         multi_out_data.append(D.make_multi_output_data(fold))
     return multi_out_data
 
-idx = 1
-for tr, v, te_x, te_y in zip(train, val, test_x, test_y):
-    print(f"{idx}")
-    idx += 1
-    print(f"train fold {tr.shape}")
-    print(f"val fold {v.shape}")
-    print(f"test input fold {te_x.shape}")
-    print(f"test output fold {te_y.shape}")
 
-print("Pre scale example")
-print(test_y[2][0])
+if __name__ == "__main__":
 
-scaled_train, _ = standardize_data(train)
-scaled_val, _ = standardize_data(val)
-scaled_test_x, _ = standardize_data(test_x)
-scaled_test_y, test_y_scalers = standardize_data(test_y)
+    train, val, test_x, test_y = split_data(D, H)
 
-print(scaled_test_y[2][0])
+    idx = 1
+    for tr, v, te_x, te_y in zip(train, val, test_x, test_y):
+        print(f"{idx}")
+        idx += 1
+        print(f"train fold {tr.shape}")
+        print(f"val fold {v.shape}")
+        print(f"test input fold {te_x.shape}")
+        print(f"test output fold {te_y.shape}")
 
-rescaled_test_y = destandardize_data(scaled_test_y, test_y_scalers)
-print(rescaled_test_y[2][0])
+    print("Pre scale example")
+    print(test_y[2][0])
 
-padded_scaled_train = pad_data(scaled_train)
-padded_scaled_test_x = pad_data(scaled_test_x, offset=1)
+    scaled_train, _ = standardize_data(train)
+    scaled_val, _ = standardize_data(val)
+    scaled_test_x, _ = standardize_data(test_x)
+    scaled_test_y, test_y_scalers = standardize_data(test_y)
 
-print(f"padded scaled train : {len(padded_scaled_train)}")
-for fold in padded_scaled_train:
-    print(f"padded scaled train : {fold.shape}")
+    print(scaled_test_y[2][0])
 
-print(f"padded scaled test x: {len(padded_scaled_test_x)}")
-for fold in padded_scaled_test_x:
-    print(f"padded scaled test x: {fold.shape}")
+    rescaled_test_y = destandardize_data(scaled_test_y, test_y_scalers)
+    print(rescaled_test_y[2][0])
+
+    padded_scaled_train = pad_data(scaled_train)
+    padded_scaled_test_x = pad_data(scaled_test_x, offset=1)
+
+    print(f"padded scaled train : {len(padded_scaled_train)}")
+    for fold in padded_scaled_train:
+        print(f"padded scaled train : {fold.shape}")
+
+    print(f"padded scaled test x: {len(padded_scaled_test_x)}")
+    for fold in padded_scaled_test_x:
+        print(f"padded scaled test x: {fold.shape}")
 
 
-print(f"Scaled val len: {len(scaled_val)}")
-print(f"Scaled val shape: {scaled_val[0].shape}")
-multi_out_scaled_val = prepare_output_data(scaled_val)
-print(f"Multi out scaled val len: {len(multi_out_scaled_val)}")
-print(f"Multi out scaled val shape: {multi_out_scaled_val[0].shape}")
+    print(f"Scaled val len: {len(scaled_val)}")
+    print(f"Scaled val shape: {scaled_val[0].shape}")
+    multi_out_scaled_val = prepare_output_data(scaled_val)
+    print(f"Multi out scaled val len: {len(multi_out_scaled_val)}")
+    print(f"Multi out scaled val shape: {multi_out_scaled_val[0].shape}")
 
-print(f"Scaled test_y len: {len(scaled_test_y)}")
-print(f"Scaled test_y shape: {scaled_test_y[0].shape}")
-multi_out_scaled_test_y = prepare_output_data(scaled_test_y)
-print(f"Multi out scaled test_y len: {len(multi_out_scaled_test_y)}")
-print(f"Multi out scaled test_y shape: {multi_out_scaled_test_y[0].shape}")
+    print(f"Scaled test_y len: {len(scaled_test_y)}")
+    print(f"Scaled test_y shape: {scaled_test_y[0].shape}")
+    multi_out_scaled_test_y = prepare_output_data(scaled_test_y)
+    print(f"Multi out scaled test_y len: {len(multi_out_scaled_test_y)}")
+    print(f"Multi out scaled test_y shape: {multi_out_scaled_test_y[0].shape}")
 
-# make input use the names.
-# make model use masking.
-# train model on each validation fold repreat for each regularizer experiment.
-# save results.
+    # make input use the names.
+    # make model use masking.
+    # train model on each validation fold repreat for each regularizer experiment.
+    # save results.
