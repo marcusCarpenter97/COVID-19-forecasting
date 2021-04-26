e84aeb331222808d2afa2959454f10a9fb845cf0
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2021-04-26 21:10:46 +0100

    Refocatored models into model.

--- COVID-19/model.py
+++ COVID-19/model.py
@@ -8,7 +8,7 @@ class EncoderBlock(layers.Layer):
     and creates a learned representation to be processed further in the model.
     """
     def __init__(self, rnn_units, rnn_layer, rnn_activation, pad_val, l1=0, l2=0, dropout=0):
-        super(EncoderBlock, self).__init__()
+        super().__init__()
         regularizer = tf.keras.regularizers.L1L2(l1=l1, l2=l2)
 
         self.mask_layer = None
@@ -20,7 +20,6 @@ class EncoderBlock(layers.Layer):
         self.hidden_dense = layers.Dense(1, name="name_encoder")
 
     def call(self, inputs):
-        mask = None
         if self.mask_layer:
             masked_inputs = self.mask_layer(inputs[0])
 
@@ -34,7 +33,7 @@ class MultiOutputRNN(keras.Model):
     Multi output RNN model with individual weights on the output nodes.
     """
     def __init__(self, output_size, rnn_units, rnn_layer, rnn_activation, l1, l2, dropout, pad_val=None):
-        super(RNNMultiOutputIndividual, self).__init__()
+        super().__init__()
         self.encoder = EncoderBlock(rnn_units, rnn_layer, rnn_activation, pad_val, l1, l2, dropout)
 
         self.c_out = layers.Dense(output_size, name="confirmed")
