3331479c4fb51cf1d2b2c849b7486210ec6856fe
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2021-05-03 13:21:21 +0100

    Made function for creating box plots from real model results. Only one validation fold at a time.

--- COVID-19/result_viewer.py
+++ COVID-19/result_viewer.py
@@ -1,8 +1,29 @@
+import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-def make_box_plot(dataA, dataB):
+def load_errors(fold, reg, ens, model, error, partition):
+    result_dir = "cross_val_results"
+    file_name = f"fold_{fold}_reg_{reg}_ens_{ens}_{model}_{error}_{partition}"
+
+    file_path = os.path.join(result_dir, file_name)
+    with np.load(file_path) as data:
+        return data["arr_0"]  # This assumes all files are contained in arr_0. Possible KeyError.
+
+def format_errors(model, error, partition):
+    ensemble_size = 10
+    regularizers = 6
+    results = []
+    for regularizer in range(regularizers):
+        mean_error = []
+        for trial in range(ensemble_size):
+            model_errors = load_errors(0, regularizer, trial, model, error, partition)
+            mean_error.append(np.mean(model_errors))
+        results.append(mean_error)
+    return np.stack(results).T
+
+def make_box_plot(dataA, dataB, fold):
     def draw_plot(data, offset,edge_color, fill_color):
         pos = np.arange(data.shape[1])+offset
         bp = ax.boxplot(data, positions= pos, widths=0.3, showmeans=True, meanline=True, patch_artist=True, manage_ticks=False)
@@ -19,39 +40,12 @@ def make_box_plot(dataA, dataB):
 
     ax.set_xticklabels(["No reg", "L1", "L2", "Dropout", "ElasticNet", "All regs"])
     ax.set_ylabel("RMSE")
-    ax.set_title("Comparison of models for validation fold X")
+    ax.set_title(f"Comparison of models for validation fold {fold}")
     ax.legend([bpA["boxes"][0], bpB["boxes"][0]], ["LSTM", "GRU"])
 
 if __name__ == "__main__":
-
-    data1 = np.random.rand(10)
-    print(f"Mean: {np.mean(data1)} Variance: {np.var(data1)}")
-    data2 = np.random.rand(10)
-    print(f"Mean: {np.mean(data2)} Variance: {np.var(data2)}")
-    data3 = np.random.rand(10)
-    print(f"Mean: {np.mean(data3)} Variance: {np.var(data3)}")
-    data4 = np.random.rand(10)
-    print(f"Mean: {np.mean(data4)} Variance: {np.var(data4)}")
-    data5 = np.random.rand(10)
-    print(f"Mean: {np.mean(data5)} Variance: {np.var(data5)}")
-    data6 = np.random.rand(10)
-    print(f"Mean: {np.mean(data6)} Variance: {np.var(data6)}")
-    data7 = np.random.rand(10)
-    print(f"Mean: {np.mean(data7)} Variance: {np.var(data7)}")
-    data8 = np.random.rand(10)
-    print(f"Mean: {np.mean(data8)} Variance: {np.var(data8)}")
-    data9 = np.random.rand(10)
-    print(f"Mean: {np.mean(data9)} Variance: {np.var(data9)}")
-    data10 = np.random.rand(10)
-    print(f"Mean: {np.mean(data10)} Variance: {np.var(data10)}")
-    data11 = np.random.rand(10)
-    print(f"Mean: {np.mean(data11)} Variance: {np.var(data11)}")
-    data12 = np.random.rand(10)
-    print(f"Mean: {np.mean(data12)} Variance: {np.var(data12)}")
-
-    dataA = np.stack([data1, data3, data5, data7, data9, data11]).T
-    dataB = np.stack([data2, data4, data6, data8, data10, data12]).T
-
-    make_box_plot(dataA, dataB)
+    gru_errors = format_errors("gru", "rmse", "test")
+    lstm_errors = format_errors("lstm", "rmse", "test")
+    make_box_plot(gru_errors, lstm_errors, 0)
 
     plt.show()
