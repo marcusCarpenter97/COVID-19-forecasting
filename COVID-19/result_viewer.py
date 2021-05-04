180a98a3a22e6c6ef338b457f259e9ae82da1365
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2021-05-04 10:46:31 +0100

    Implemented method for plotting all predictions made along with ensembles and error values.

--- COVID-19/result_viewer.py
+++ COVID-19/result_viewer.py
@@ -1,15 +1,19 @@
 import os
+import csv
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
-def load_errors(fold, reg, ens, model, error, partition):
+def build_file_name(fold, reg, ens, model, error, partition):
+    return f"fold_{fold}_reg_{reg}_ens_{ens}_{model}_{error}_{partition}"
+
+def load_npz(file_name):
     result_dir = "cross_val_results"
-    file_name = f"fold_{fold}_reg_{reg}_ens_{ens}_{model}_{error}_{partition}"
 
     file_path = os.path.join(result_dir, file_name)
     with np.load(file_path) as data:
-        return data["arr_0"]  # This assumes all files are contained in arr_0. Possible KeyError.
+        ret = [data[i] for i in data]  # There can be multiple arrays in a file.
+    return np.squeeze(np.array(ret))
 
 def format_errors(fold, model, error, partition):
     ensemble_size = 10
@@ -18,7 +22,8 @@ def format_errors(fold, model, error, partition):
     for regularizer in range(regularizers):
         mean_error = []
         for trial in range(ensemble_size):
-            model_errors = load_errors(fold, regularizer, trial, model, error, partition)
+            file_name = build_file_name(fold, reg, ens, model, error, partition)
+            model_errors = load_npz(file_name)
             mean_error.append(np.mean(model_errors))
         results.append(mean_error)
     return np.stack(results).T
@@ -53,5 +58,52 @@ def make_box_plots():
         lstm_errors = format_errors(fold, "lstm", "rmse", "test")
         make_box_plot(lstm_errors, gru_errors, fold)
 
+def load_location_names():
+    path = os.path.join("cross_val_results", "country_names.csv")
+    with open(path, "r", newline="") as csv_file:
+        reader = csv.reader(csv_file)
+        results = [row for row in reader]
+    return results[0]
+
+def make_ensemble_plots(model, partition):
+    preds = []
+    errs = []
+    ensemble_size = 10
+    folds = 14
+    regularizers = 6
+    loc_names = load_location_names()
+    display = (0, 10, 11)
+    sub_titles = ["Confirmed", "Deceased", "Recovered"]
+    orig = load_npz("test_y")
+
+    for fold in range(folds):
+        for reg in range(regularizers):
+            for e in range(ensemble_size):
+                pred_file_name = build_file_name(fold, reg, e, model, partition, "pred")
+                error_file_name = build_file_name(fold, reg, e, model, "rmse", partition)
+                preds.append(load_npz(pred_file_name))
+                errs.append(load_npz(error_file_name))
+            avg_pred = np.mean(preds, axis=0)
+            avg_errs = np.mean(errs, axis=0)
+
+            for curr_loc in len(loc_names):
+                fig, axes = plt.subplots(1,3)
+                for idx, axis in enumerate(axes):
+                    for p in preds:
+                        axis.plot(p[curr_loc].T[idx], color="moccasin", label="Ensemble")
+                    axis.plot(orig[fold][curr_loc].T[idx], color="blue", label="Original")
+                    axis.plot(avg_pred[curr_loc].T[idx], linestyle="--", color="red", label=f"Average {model.upper()}")
+                    axis.set_title(f"{sub_titles[idx]} : {round(avg_errs[curr_loc][idx], 3)} RMSE")
+                handles, labels = axis.get_legend_handles_labels()
+                axes[0].set_ylabel("People")
+                axes[1].set_xlabel("Days")
+                axes[2].legend([handle for i, handle in enumerate(handles) if i in display],
+                               [label for i, label in enumerate(labels) if i in display], loc=0)
+                fig.suptitle(loc_names[curr_loc])
+                fig_path = os.path.join("plots", f"fold_{fold}_reg_{reg}_{model}_{partition}_{loc_name[curr_loc]}")
+                plt.savefig(fig_path)
+
 if __name__ == "__main__":
-    make_box_plots()
+    #make_box_plots()
+    make_ensemble_plots("gru", "test")
+    make_ensemble_plots("lstm", "test")
